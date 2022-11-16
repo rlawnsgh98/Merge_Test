46a90c346e2a3e5cffe0071a7e75df1fc4dacdf7
rlawnsgh98 <hoho0084@gmail.com>
2022-11-16 17:05:15 +0900

    ㅇ
    ㅇ

--- workspace/mergetest/src/mergetest/MergeTest.java
+++ workspace/mergetest/src/mergetest/MergeTest.java
@@ -3,6 +3,6 @@ package mergetest;
 public class MergeTest {
 	public static void main(String[] args) {
 		System.out.println("hello");
-		System.out.println("김준호");
+		System.out.print("김준호");
 	}
 }
